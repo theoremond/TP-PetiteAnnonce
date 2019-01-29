ff8216ee5a7c8ad5b404311b0fea2598c9b22963
Maxime <maximebruchet21@gmail.com>
2019-01-29 19:28:40 +0100

    :construction: Le form est fait plus qu'a ajouter dans la base

---
+++ src/AppBundle/Controller/AdvertisementType.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Annonce;
+
+class AdvertisementType extends Controller
+{
+
+    /**
+     * @Route("/creer_annonce") 
+     */
+    public function create(Request $request) {
+        $annonce = new Annonce();
+
+        $form = $this->createFormBuilder(
+                $annonce,
+                array(
+                    'method' => 'POST'
+                )
+            )
+            -> add(
+                'title',
+                TextType::class,
+                array(
+                    'label' => 'Le titre de l\'annonce',
+                    'attr' => array(
+                        'placeholder' => 'Titre'
+                    )
+                )
+            )
+            -> add(
+                'price',
+                TextType::class,
+                array(
+                    'label' => 'Le prix de l\'annonce',
+                    'attr' => array(
+                        'placeholder' => 'Prix'
+                    )
+                )
+            )
+            -> add(
+                'content',
+                TextareaType::class,
+                array(
+                    'label' => 'Contenu',
+                    'attr' => array(
+                        'placeholder' => 'Contenu'
+                    )
+                )
+            )
+            -> add(
+                'zipcode',
+                TextType::class,
+                array(
+                    'label' => 'Le Code Postal',
+                    'attr' => array(
+                        'placeholder' => 'Code Postal'
+                    )
+                )
+            )
+            ->add(
+                'Valider',
+                SubmitType::class,
+                array(
+                    'label' => 'Valider',
+                )
+            )
+            ->getForm();
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $annonce = $form->getData();
+
+            $entityManager->persist($annonce);
+            $entityManager->flush();
+
+            // return to -> annonce;
+        } else {
+            return $this->render(
+                'new.html.twig',
+                array(
+                    'form' => $form->createView(),
+                )
+            );
+        }
+    }
+}
